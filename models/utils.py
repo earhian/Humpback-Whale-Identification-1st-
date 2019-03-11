2bcb126fb255670b0da57b8a104e47267a8c3a17
earhian <992747523@qq.com>
2019-03-11 20:26:46 +0800

    remove lovasz

--- models/utils.py
+++ models/utils.py
@@ -105,7 +105,7 @@ def sigmoid_loss(results, labels, topk=10):
     batch_size, class_num = results.shape
     labels = labels.view(-1, 1)
     one_hot_target = torch.zeros(batch_size, class_num + 1).cuda().scatter_(1, labels, 1)[:, :5004 * 2]
-    lovasz_loss = lovasz_hinge(results, one_hot_target)
+    #lovasz_loss = lovasz_hinge(results, one_hot_target)
     error = torch.abs(one_hot_target - torch.sigmoid(results))
     error = error.topk(topk, 1, True, True)[0].contiguous()
     target_error = torch.zeros_like(error).float().cuda()
@@ -117,7 +117,7 @@ def sigmoid_loss(results, labels, topk=10):
     results_nonew = results[torch.arange(0, len(results))[indexs_new], labels[indexs_new]].contiguous()
     target_nonew = torch.ones_like(results_nonew).float().cuda()
     nonew_loss = nn.BCEWithLogitsLoss(reduce=True)(results_nonew, target_nonew)
-    return nonew_loss + error_loss + lovasz_loss * 0.5
+    return nonew_loss + error_loss
 
 if __name__ == '__main__':
     results = torch.randn((4, 5004)).cuda()
